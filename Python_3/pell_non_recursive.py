6899d59ca5f4f1932dd8fc97866d5741242f2435
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-06-29 00:19:36 +0200

    Pell Recursive and Non-Recursive Methods

---
+++ Python_3/pell_non_recursive.py
@@ -0,0 +1,8 @@
+# Non recursive function
+def pell_non_recursive(n):
+    pell_seq = [0,1]
+    for k in range (2, n+1):
+        pell_seq.append(2*pell_seq[k-1]+pell_seq[k-2])
+    return pell_seq[n]
+# Calling the Non-recursive function
+pell_non_recursive(0)
---
+++ Python_3/pell_recursive.py
@@ -0,0 +1,10 @@
+# Recursive function
+def pell_recursive(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return 2*pell_recursive(n-1)+pell_recursive(n-2)
+# Calling the Recursive function
+pell_recursive(20)
